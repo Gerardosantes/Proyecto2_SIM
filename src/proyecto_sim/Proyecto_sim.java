27b3f29bd594640f0da5532e3f1c2e29a5bfb91a
Gerardosantes <perezzg816@gmail.com>
2026-05-16 16:37:41 -0600

    agrege 2 librerias e le di nombres a unas variables

--- src/proyecto_sim/Proyecto_sim.java
+++ src/proyecto_sim/Proyecto_sim.java
@@ -1,10 +1,24 @@
 
 package proyecto_sim;
 
+import java.util.Scanner;
+import java.util.Random;
+
 public class Proyecto_sim {
 
   
     public static void main(String[] args) {
+
+           Scanner leer = new Scanner(System.in);
+        Random random = new Random();
+
+
+          int opcion;
+        double saldo = 0;
+        double deposito;
+        double retiro;
+        int folio;
+
         
     }
     
